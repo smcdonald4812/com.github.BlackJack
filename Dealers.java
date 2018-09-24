6296893fb3776eeff49e3f5fe49e86bd78a8a5eb
smcdonald4812 <36778056+smcdonald4812@users.noreply.github.com>
2018-09-24 16:10:09 -0400

    Update Dealers.java

--- Dealers.java
+++ Dealers.java
@@ -1,5 +1,7 @@
+//Is used in conjunction with Bots and Beings for Dealer class
+
 interface Dealers {
 	void setStartBlackJack();
 	boolean isStartBlackJack();
-	//more to come when thought of
+	//more to come if needed
 }
