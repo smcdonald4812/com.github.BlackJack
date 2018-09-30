a4ade73396ce749819d8fb66bd78bb77ef7ae70f
smcdonald4812 <36778056+smcdonald4812@users.noreply.github.com>
2018-09-30 15:23:29 -0400

    Delete Dealers.java

--- Interfaces/Dealers.java
+++
@@ -1,7 +0,0 @@
-interface Dealers {
-	void setStartBlackJack(boolean startBlackJack);
-	boolean isStartBlackJack();
-	void setCardShowing(boolean cardShowing);
-	boolean isCardShowing();
-	//more to come when thought of
-}
